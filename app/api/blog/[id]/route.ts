b23941b2b122cfa53aa0af34ba8d984580b42a10
alfaruqii <bumbblechover@gmail.com>
2023-06-10 17:14:52 +0700

    adding PUT & DELETE method inside dynamic route

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -21,3 +21,45 @@ export const GET = async (req:Request,res:NextResponse) => {
     await prisma.$disconnect()
   }
 }
+
+export const PUT = async (req:Request,res:NextResponse) => {
+  try {
+    const {title,description} = await req.json()
+    const id = req.url.split("/blog/")[1]
+    await main()
+    const post = await prisma.posts.update({
+      where:{
+        id
+      },
+      data:{
+        title,
+        description
+      }
+    })
+    if(!post) return NextResponse.json({message:"Error"},{status:500})
+    return NextResponse.json({message:"Succes",post},{status:200})
+  } catch (error) {
+    return NextResponse.json({message:"Error",error},{status:500})
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
+
+export const DELETE = async (req:Request,res:NextResponse) => {
+  try {
+    const id = req.url.split("/blog/")[1]
+    await main()
+    const post = await prisma.posts.delete({
+      where:{
+        id
+      },
+    })
+    if(!post) return NextResponse.json({message:"Error"},{status:500})
+    return NextResponse.json({message:"Deleting Succes",post},{status:200})
+  } catch (error) {
+    return NextResponse.json({message:"Error",error},{status:500})
+  } finally {
+    await prisma.$disconnect()
+  }
+}
