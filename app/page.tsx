d73b46dfe73793c4f3f19634f92450337ec27f5d
alfaruqii <bumbblechover@gmail.com>
2023-06-11 14:11:30 +0700

    adding fetchBlogs inside page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+const fetchBlogs = async () => {
+  const posts = await fetch("localhost:3000/blog/api",{
+    next:{
+      revalidate:10
+    }
+  })
+  const res = await posts.json()
+  return res.posts
+}
+
 export default function Home() {
   return (
     <div>
