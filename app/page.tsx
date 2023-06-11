4c962f30ba1efe25bc5b79a63edb4bfc52895250
alfaruqii <bumbblechover@gmail.com>
2023-06-11 17:17:27 +0700

    add style in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link"
+
+export type Post = {
+  id: string,
+  title: string,
+  description: string,
+  date: string
+}
+
 const fetchBlogs = async () => {
-  const posts = await fetch("localhost:3000/blog/api",{
+  const posts = await fetch("http://localhost:3000/api/blog",{
     next:{
       revalidate:10
     }
@@ -8,10 +17,37 @@ const fetchBlogs = async () => {
   return res.posts
 }
 
-export default function Home() {
+export default async function Home() {
+  const posts = await fetchBlogs()
   return (
-    <div>
-      Hello World
-    </div>
+    <main>
+      <div className="border-2 border-white p-4 w-fit mx-auto mt-4 rounded-sm">
+        <p className="font-bold text-2xl">
+          Welcome to my blog 📝
+        </p>
+      </div>
+      <div className="border border-white p-2 w-fit mx-auto mt-3 rounded-sm">
+        <button>
+          <Link href="/blog/new">
+            Add new blog post
+          </Link>
+        </button>
+      </div>
+      <div>
+        {posts?.map((post:Post) => {
+          return <div className="w-fit mx-auto p-3  first:mt-6 mt-3 border" key={post.id}>
+            <h1 className="text-xl font-medium">
+              Title : {post.title}
+            </h1>
+            <p className="">
+              Description : {post.description}
+            </p>
+            <p className="mt-3 text-xs text-slate-400">
+              Uploaded at : {new Date(post.date).toLocaleString()}
+            </p>
+          </div>
+        })}
+      </div>
+    </main>
   )
 }
